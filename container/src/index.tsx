4f00f8be3a3ecb87983e47083f2c8ca0961ce90e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-10-19 13:54:40 -0500

    feat: init project webpack with typescript

---
+++ container/src/index.tsx
@@ -0,0 +1,3 @@
+console.log("Hello to app");
+
+export {}
---
+++ container/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: "./src/index.tsx",
+	devtool: "eval-source-map",
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx']
+	},
+	mode: 'development',
+	module: {
+		rules: [{
+			test: /\.tsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/
+		}, {
+			test: /\.(scss)$/,
+			use: [{
+				loader: 'style-loader', // inject CSS to page
+			}, {
+				loader: 'css-loader', // translates CSS into CommonJS modules
+			}, {
+				loader: 'postcss-loader', // Run post css actions
+				options: {
+					postcssOptions: {
+						plugins: function () { // post css plugins, can be exported to postcss.config.js
+							return [
+								require('autoprefixer')
+							];
+						}
+					}
+				}
+			}, {
+				loader: 'sass-loader' // compiles Sass to CSS
+			}]
+		}, {
+			test: /\.svg$/i,
+			type: 'asset',
+			resourceQuery: /url/, // *.svg?url
+		}, {
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+			use: ['@svgr/webpack'],
+		},]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: 'index.html'
+		})
+	]
+}
